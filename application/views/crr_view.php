1971792a4ad3bed4942cda5d8fa797ca3ee29b3b
monixis <snh.monish@gmail.com>
2015-07-07 09:58:56 -0400

    dashboard html and css update

--- application/views/crr_view.php
+++ application/views/crr_view.php
@@ -13,12 +13,17 @@
 		<script src="http://library.marist.edu/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
 		<script type="text/javascript" src="./js/jquery-1.11.3.min.js"></script> 
 		<script type="text/javascript" src="./js/jquery-ui.js"></script>
-		<link rel="stylesheet" href="./styles/jquery-ui.css" type="text/css" /> 
+		 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 		<script>
     		$(document).ready(function(){
     			$("#datepicker").datepicker({
     				minDate : "+0"
     			});
+    			$("#datepicker").datepicker( "setDate", new Date());
+    			var date = $('input#datepicker').val();
+				var find = '/';
+				var re = new RegExp (find, 'g');
+				date = date.replace(re,'');
     		})
     	</script>
 		<script type="text/javascript" charset="utf-8">
@@ -63,8 +68,8 @@
   					<td>= Reservation Expired</td>
   				</tr>
   			</table-->
-  			<p id="pickDate">Pick a date to view available rooms: <input type="text" name="viewDate" id="datepicker" value="<?php $date = getdate(); echo($date["mon"]. "/" . $date["mday"]. "/" . $date["year"]); ?>" /></p>
- 			<p id="viewDate">Date Being Viewed: 
+  			<p id="pickDate">Select a date: <input type="text" name="viewDate" id="datepicker" value="" /></p>
+ 			<!--p id="viewDate">Date Being Viewed: 
  				<script type="text/javascript">
  				document.write($("#datepicker").val());
  				$( "#datepicker" ).change(function() {
@@ -72,7 +77,7 @@
  					document.getElementById("viewDate").innerHTML = "Date Being Viewed: " + $("#datepicker").val();
 				});
  				</script> 
- 			</p>
+ 			</p-->
 		</div>
 		
 		<div id="dashboard">
@@ -87,19 +92,25 @@
   					<th>Time</th>
   					  
   				<?php 
+  				 $formatDate = date("m/d/Y"); 
   				 $totalrooms = 0;
+				 $cnt = 0;
+				 $a_rooms = array();
   				foreach ($rooms as $row) {
 					  $roomNum = $row -> roomNum;
 					  $totalrooms =  $totalrooms +1; 
 				?>
-				<th><?php echo $roomNum; ?></th>
-				 <?php  } ?>
+				<th id="<?php echo $cnt; ?>"><?php echo $roomNum; ?></th>
+				 <?php  
+					$a_rooms[$cnt] = $roomNum;
+					$cnt= $cnt +1; 
+				} ?>
   				</tr>
   				
   				<?php 
   				$currtime = $stime;
 				$y = 0;
-				$time = "am";
+				//$time = "am";
   				while ($y < $totalhrs){
   					if ($y == 0){
   						$currtime = $stime;
@@ -107,7 +118,7 @@
   						$currtime = $currtime + 1 ;
   					}
   									
-					if ($currtime == 13)
+					/*if ($currtime == 25)
 					{
 						$currtime = 1;
 						if ($time == "am"){
@@ -116,15 +127,22 @@
 							$time = "am";
 						}
 						
-					}
+					}*/
+					
+					if ($currtime == 25){
+						$currtime = 1;
+							}
+					
 					$y = $y + 1;
-					$currtime = $currtime . " " . $time;
+					$currtime1 = $currtime . ":" . "00";
   				?>	<tr>
-  						<td><?php echo $currtime; ?></td>
-  						
-  						<?php 
+  						<td><?php echo $currtime1; ?></td>
+
+ 						<?php 
 							for ($i =0; $i < $totalrooms ; $i++){
-								?><td></td>
+						?>
+					
+						<td class="slots" id="<?php echo $formatDate.$a_rooms[$i].$currtime ;?>"></td>
 						<?php
 							}					  						
   						?>
@@ -140,12 +158,39 @@
 			<button id="resButton" onclick="location.href='<?php echo base_url("?c=crr&m=reserveform"); ?>'">
      Reserve Form</button>
     	</div-->	
+			<script type="text/javascript">
+				$('#datepicker').change(function(){
+					var date = $('input#datepicker').val();
+					var find = '/';
+					var re = new RegExp (find, 'g');
+					date = date.replace(re,'');
+					$('input#formatDate').attr('value',date);
+					var id ='';
+					//alert(date);
+					$('td.slots').each(function(){
+					id = $(this).attr('id');
+					id=id.substring(8);
+					id = date.concat(id);
+					$(this).attr('id', id);	
+					});
+					
+					
+				});
+				
+				$('td').click(function(){
+					alert($(this).attr('id'));
+				})
+				
+				$('#datepicker').load(function(){
+					alert('Monish');
+				});
+			</script>
 				
 			<div class="bottom">
 				<p class = "foot">
 					Marist College, 3399 North Road, Poughkeepsie, NY 12601; 845-575-3000
 					<br />
-					&#169; Copyright 2007-2014 Marist College. All Rights Reserved.
+					&#169; Copyright 2007-2015 Marist College. All Rights Reserved.
 
 					<a href="http://www.marist.edu/disclaimers.html" target="_blank" >Disclaimers</a> | <a href="http://www.marist.edu/privacy.html" target="_blank" >Privacy Policy</a> 
 				</p>
