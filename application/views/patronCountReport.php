12f53112a733e3238fa972f400f8863fac4060a6
Deep Dand <deep.dand1992@gmail.com>
2017-09-12 11:54:24 -0400

    added primary patron email ID and room number

--- application/views/patronCountReport.php
+++ application/views/patronCountReport.php
@@ -2,22 +2,37 @@
 <style>
 	td {width: 120px;}
 </style>
-<table id="resTable" style="width: 300px; margin-left:100px;">
-	<thead>
-		<tr>
-			<th>Time</th>
-			<th>Total Patron Count</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php 
-		foreach ($hours as $row1) {
-			$hours = $row1 -> time;
-			$patronCount = $row1 -> patroncount;	
-		?> 
-		<tr><td><?php echo $hours; ?></td><td><?php echo $patronCount; ?></td></tr>
+
 		<?php
-		}
+		if($hours!= null) { ?>
+			<table id="resTable" style="width: 300px; margin-left:100px;">
+				<thead>
+					<tr>
+						<th>Room Number</th>
+						<th>Time</th>
+						<th>Total Patron Count</th>
+						<th>Primary Patron Email</th>
+					</tr>
+				</thead>
+				<tbody>
+			<?php
+		foreach ($hours as $row1) {
+			$roomNum = $row1 -> roomNum;
+			$hours = $row1 -> time;
+			$patronCount = $row1 -> patroncount;
+			$resEmail = $row1 -> resEmail;
+		?>
+		<tr>
+			<td><?php echo $roomNum; ?></td>
+			<td><?php echo $hours; ?></td>
+			<td><?php echo $patronCount; ?></td>
+			<td><?php echo $resEmail; ?></td>
+		</tr>
+		<?php
+	}}
+	else {
+		echo "<h2>No bookings made.<h2>";
+	}
 		?>
 	</tbody>
 </table>
